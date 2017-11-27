d7fda73168896fe5a056e03acfd2a7b761d85eea
sveppalicious <sverrir.sigurdarson@gmail.com>
2017-11-27 18:47:00 +0000

    added comments

--- day1/setup.sh
+++ day1/setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+# I took the utils.sh script from
+# https://natelandau.com/bash-scripting-utilities/
+# e_header, e_note, e_bold, seek_confirmation, is_confirmed,
+# e_success, e_error and type_exists
+# are all helper functions that I import from utils.sh
 source $HOME/HGOP/day1/utils.sh
 e_header "Setup Script"
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 echo "$dt"
 e_note "This is a setup script for
 T-542-HGOP at Reykjavík University."
+# $USER and $OSTYPE are environment variables
 echo "Hello," $USER
 echo "You are running on," $OSTYPE
 seek_confirmation "Do you want to check your setup?"
@@ -17,6 +23,7 @@ if is_confirmed; then
     seek_confirmation "Install git?"
     if is_confirmed; then
       e_note "Installing git"
+      # Only works for Arch Linux
       sudo pacman -S git
     else
       e_error "Aborting"
@@ -31,6 +38,7 @@ if is_confirmed; then
     seek_confirmation "Install Atom?"
     if is_confirmed; then
       e_note "Installing Atom"
+      # Only works for Arch Linux
       sudo pacman -S atom
     else
       e_error "Aborting"
